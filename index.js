e578bde9d447701ea1573bddd9372e6cd032aaed
unknown <khanmk2000@gmail.com>
2023-01-30 03:31:00 +0530

    commit message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURI = "mongodb+srv://mohsin_k:<2v7LqQP7gvWn4i2i >@cluster0.migeo4q.mongodb.net/?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://mohsin_k:<qO9VBQHOlQOwaV7e>@cluster0.migeo4q.mongodb.net/?retryWrites=true&w=majority"
 
 try{
     mongoose.connect(
@@ -8,7 +8,16 @@ try{
         ()=>{
             console.log("mongodb connected");
         }
-        );
+        ).catch(e=>console.log(e));;
 }catch(e){
     console.log(e); 
 }
+const User = require("./models/User");
+const user = new User({
+    Email: "mk@gmail.com", 
+    Password:"12345"
+});
+
+user.save().then(()=>{
+    console.log("Success");
+});
